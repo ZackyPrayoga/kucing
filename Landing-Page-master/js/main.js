abdf779f0485c1313d672f8d4a68a3a57f52bbdf
ZackyPrayoga <114891585+ZackyPrayoga@users.noreply.github.com>
2023-08-10 08:13:29 +0700

    Add files via upload

---
+++ Landing-Page-master/js/main.js
@@ -0,0 +1,9 @@
+// const animation = document.querySelectorAll(".jenis-kucing");
+// animation.forEach(e => {
+//     e.addEventListener('mouseover', function(){
+//         e.classList.add('animate__animated', 'animate__pulse');
+//     })
+//     e.addEventListener('mouseout', function(){
+//         e.classList.remove('animate__animated', 'animate__pulse');
+//     })
+// })
